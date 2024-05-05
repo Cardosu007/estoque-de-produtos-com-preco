29228e896193c578d3c19ee83c8afd2f37f69ef5
W Cardoso <cardosu7@gmail.com>
2024-05-05 11:54:50 -0300

    finalizado

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        
+
+        EstoqueProdutos estoque = new EstoqueProdutos();
+
+        estoque.exibirProdutos();
+
+        estoque.adicionarProduto(1L, "Produto A", 10, 50.0);
+        estoque.adicionarProduto(2L, "Produto B", 15, 15.0);
+        estoque.adicionarProduto(3L, "Produto C", 20, 555.0);
+        estoque.adicionarProduto(7L, "Produto D", 30, 10.0);
+        estoque.adicionarProduto(4L, "Produto E", 80, 255.0);
+
+        System.out.println(estoque.calculaValorTotalEstoque());
+        estoque.exibirProdutos();
+
     }
 }
--- src/Produto.java
+++ src/Produto.java
@@ -4,7 +4,7 @@ public class Produto {
     private int quantidade;
     private double preco;
 
-    public Produto(, String nome, int quantidade, double preco) {
+    public Produto(String nome, int quantidade, double preco) {
         this.nome = nome;
         this.quantidade = quantidade;
         this.preco = preco;
@@ -32,5 +32,14 @@ public class Produto {
 
     public void setPreco(double preco) {
         this.preco = preco;
+    }
+
+    @Override
+    public String toString() {
+        return "Produto{" +
+                "nome='" + nome + '\'' +
+                ", quantidade=" + quantidade +
+                ", preco=" + preco +
+                '}';
     }
 }
